fd7c14ead169a6e4e28bbb075527b40f826cd12c
lybahoang <lybahoang1@gmail.com>
2025-06-15 21:17:25 +0700

    PC8

--- Chapter17/programming_challenges/PC8/List.h
+++ Chapter17/programming_challenges/PC8/List.h
@@ -1,5 +1,7 @@
 #ifndef LIST_H
 #define LIST_H
+#include <iostream>
+using namespace std;
 
 template <class T>
 class List
--- Chapter17/programming_challenges/PC8/main.cpp
+++ Chapter17/programming_challenges/PC8/main.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int main()
 {
+    // Create an integer list and a double list.
+    List<int> integerList;
+    List<double> doubleList;
 
-    // Create an integer list.
-    List integerList(SIZE);
+    // Initializes the lists with some values.
+    for (int count = 0; count < 5; count++)
+    {
+        integerList.appendNode(count + 25);
+        doubleList.appendNode(count * 2.7);
+    }
+
+    // Display the integer list.
+    cout << "Here are values in integerList.\n";
+    integerList.displayList();
+
+    // Display values in the doubleList.
+    cout << "\nHere are values in doubleList.\n";
+    doubleList.displayList();
+
+    return 0;
 }
